3b2238f2c655c4b4e2dad76b9870d3347d65ad52
Monarch Sharma <monarchsharma.m20@gmail.com>
2026-05-08 02:06:56 +0530

    considering this be final code

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from Flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('../templates/index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
