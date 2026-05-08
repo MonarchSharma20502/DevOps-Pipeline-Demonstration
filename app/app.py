0ea889e8bf05b0a0be665ebc61ad403ebdd2b290
Monarch Sharma <monarchsharma.m20@gmail.com>
2026-05-08 21:17:33 +0530

    DevOps Pipeline

--- app/app.py
+++ app/app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # return "render_template('index.html')"
+    return "Hello, World! This is a Flask application running in a Docker container."
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
