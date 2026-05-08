6c277d6d731b3dd93b0f82e0bf3e767a7a331d3c
Monarch Sharma <monarchsharma.m20@gmail.com>
2026-05-08 13:24:57 +0530

    DevOps Pipeline

--- app/app.py
+++ app/app.py
@@ -1,9 +1,8 @@
-from Flask import Flask, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('../templates/index.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9000)
-# 0.0.0.0 - anyIP
